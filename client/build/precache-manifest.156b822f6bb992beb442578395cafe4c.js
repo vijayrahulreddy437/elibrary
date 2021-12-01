afa127abbbac4ba5b6a85cbadbe4da4236642963
Vijayrahul <vijayrahu437@gmail.com>
2021-12-01 22:28:33 +0530

    first commit

---
+++ client/build/precache-manifest.156b822f6bb992beb442578395cafe4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3796d8c4d25affeaf0c5",
+    "url": "/static/css/main.a1056f7b.chunk.css"
+  },
+  {
+    "revision": "3796d8c4d25affeaf0c5",
+    "url": "/static/js/main.3796d8c4.chunk.js"
+  },
+  {
+    "revision": "837c01d672b29ba4130d",
+    "url": "/static/js/1.837c01d6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "055edb98e2d00af094486801fad79abf",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -60,14 +60,11 @@ require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
 
-
-if(process.env.NODE_ENV==="production"){
   app.use(express.static('client/build'));
 
   app.get('*', (req,res)=>{
     res.sendFile(path.resolve(__dirname, "client","build","index.html"))
   })
-}
 
 const port = process.env.PORT || 5000;
   
